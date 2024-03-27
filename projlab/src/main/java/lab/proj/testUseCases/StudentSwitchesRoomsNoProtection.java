0022d64427176dded5982785fec71fe19cd0b325
Szatmári Soma <szasomaa@gmail.com>
2024-03-27 20:35:05 +0100

    hallgató nem véd javítva

--- projlab/src/main/java/lab/proj/testUseCases/StudentSwitchesRoomsNoProtection.java
+++ projlab/src/main/java/lab/proj/testUseCases/StudentSwitchesRoomsNoProtection.java
@@ -5,13 +5,15 @@ import lab.proj.utils.IndentedDebugPrinter;
 import java.util.List;
 import java.util.Optional;
 
-public class StudentSwitchesRoomsNoProtection extends StudentSwitchesRooms {
+public class StudentSwitchesRoomsNoProtection extends OneTeacherOneStudent {
     private static final IndentedDebugPrinter Logger = IndentedDebugPrinter.getInstance();
 
     @Override
     public void runUseCase() {
         super.runUseCase();
-        assert result; // after running the base case, it should be true
+        Logger.invokeObjectMethod(IndentedDebugPrinter.MAIN, s, "UseDoor", List.of(d));
+        boolean success = s.UseDoor(d);
+        Logger.returnFromMethod(IndentedDebugPrinter.MAIN, s, "UseDoor", Optional.of(success));
         Logger.invokeObjectMethod(IndentedDebugPrinter.MAIN,
                 t1,
                 "DropOutAll",
