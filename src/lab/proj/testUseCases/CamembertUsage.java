7ff9907138d8a5085f5619f0fcb978befcbec7d5
Szatmári Soma <szasomaa@gmail.com>
2024-03-28 13:49:16 +0100

    camembert use egyszerűsítve és szépítve és bugfix rajta

--- src/lab/proj/testUseCases/CamembertUsage.java
+++ src/lab/proj/testUseCases/CamembertUsage.java
@@ -1,17 +1,30 @@
 package lab.proj.testUseCases;
 
+import lab.proj.model.Camembert;
+import lab.proj.model.Room;
+import lab.proj.model.Student;
+
 /**
  * A class representing the usage of a Camembert item in a specific use case.
  * This use case involves two teachers and one student.
  */
-public class CamembertUsage extends TwoTeachersOneStudent {
+public class CamembertUsage implements TestUseCase {
 
     /**
      * Runs the use case, invoking the superclass method and activating the Camembert item.
      */
     @Override
     public void runUseCase() {
-        super.runUseCase();
+        Student s = new Student();
+        Room r1 = new Room();
+        Camembert cm = new Camembert();
+
+        s.SetLocation(r1);
+
+        boolean camembertPickedUp = cm.PickUp(s);
+        if (!camembertPickedUp) {
+            return;
+        }
         cm.Activate();
     }
 }
